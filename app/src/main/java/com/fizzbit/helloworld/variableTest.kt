80b560bbd97b80169587fe6f80ad15beb2dc1ebc
iamhr <m_rahman_hridoy@yahoo.com>
2020-05-14 23:13:05 -0400

    Exercise 1 . Variable Test Knowledge

---
+++ app/src/main/java/com/fizzbit/helloworld/variableTest.kt
@@ -0,0 +1,36 @@
+package com.fizzbit.helloworld
+
+
+//Please write a program in which you are assigning those values to variables with the right (specific) data type:
+//
+//
+//
+//"Android Masterclass"
+//
+//13.37F
+//
+//3.14159265358979
+//
+//25
+//
+//2020
+//
+//18881234567
+//
+//true
+//
+//'a'
+//
+
+fun main(){
+
+    val details : String = "Android Masterclass"
+    val floatNumber : Float = 13.37F
+    val doubleNumber : Double = 3.14159265358979
+    val shortNumber : Short = 25
+    val intNumber : Int = 2020
+    val longNumber: Long = 18881234567
+    val isBoolean : Boolean = true
+    val charCharacter : Char = 'a'
+
+}
